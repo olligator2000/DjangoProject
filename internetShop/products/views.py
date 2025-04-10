3c5bbfa67e63daf6e92a9a031f5bc97568778eff
Студент Академии <std@itstep.loc>
2025-04-10 20:33:17 +0300

    base.html, class productategory, class product

--- internetShop/products/views.py
+++ internetShop/products/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
+    context = {
+        "title": "store"
+    }
     return render(request, "products/index.html")
 
 
 def products(request):
+    context = {
+        "title": "store - products"
+    }
     return render(request, "products/products.html")
