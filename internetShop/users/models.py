bb9ed5d97472625c6f415d469c670b5e6ce7c9f3
Студент Академии <std@itstep.loc>
2025-04-29 21:24:19 +0300

    sozdali /users/forms.py and nastroili /users/views.py

--- internetShop/users/models.py
+++ internetShop/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Дочерний класс от AbstractUser (от Джанго - auth.models)
 class User(AbstractUser):
     #Добавляем новое поле - фото(будут хранятся в media/users_images), фото пользователь может и не поставить
