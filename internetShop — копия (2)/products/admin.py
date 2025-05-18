132f99326c7e583fdac7f73ae7932bf8e9287948
olligator2000 <61042566+olligator2000@users.noreply.github.com>
2025-05-18 22:19:23 +0300

    Delete internetShop — копия (2) directory

--- internetShop — копия (2)/products/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from products.models import Product, ProductCategory
-
-admin.site.register(Product)
-admin.site.register(ProductCategory)
--- internetShop — копия (2)/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from products.views import products, products_list
-
-app_name = "products"
-
-urlpatterns = [
-    path('', products, name="index"),
-    path('list/', products_list, name="products_list"),
-]
