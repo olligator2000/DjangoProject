fc74742f47cb251bf31c2c72bfda9e11d1027d29
olligator2000 <oleg_luki@mail.ru>
2025-06-04 19:04:00 +0300

    dodelal korzinu

--- internetShop/products/admin.py
+++ internetShop/products/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from products.models import Product, ProductCategory, Basket
 
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(ProductCategory)
 admin.site.register(Basket)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name',)
+    ordering = ('-name', 'price',)
+    fields = ('name', 'image', 'short_description', ('price', 'quantity', 'category'))
+    # readonly_fields = ('short_description',)
+
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    filter = ('product',  'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
--- internetShop/users/admin.py
+++ internetShop/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from users.models import User
+from products.admin import BasketAdminInline
 
-admin.site.register(User)
 
-
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = (BasketAdminInline,)
